c02aa5d9b641c34ed9ccbf5532265cccd1fcdb41
Dauti, Elios <edauti@essex.ac.uk>
2018-10-31 10:26:53 +0000

    Test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.printIn("Daniel S A Callender");
+        System.out.println("Daniel S A Callender");
         System.out.println("Nathaniel J Robson");
         System.out.println("Callum Kleber");
-        System.out.printIn("Kyle Moody");
+        System.out.println("Kyle Moody");
         System.out.println("Added new line with git");
         System.out.println("Abit sticky");
     }
