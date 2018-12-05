05fb797813a5a1c226b9003dee1d7eea350ff082
nr17076 <nr17076@essex.ac.uk>
2018-12-05 10:51:39 +0000

    okay

--- src/MainProgramFrame.java
+++ src/MainProgramFrame.java
@@ -1,41 +1,73 @@
+
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class MainProgramFrame {
-    static class LoginFrame extends JFrame {
+public class MainProgramFrame
+{
+    public static void main( String[] args )
+    {
+        new LoginFrame();
 
-        LoginFrame(){
-        LoginFrame frame = new LoginFrame();
+
+    }
+}
+
+class LoginFrame extends JFrame
+{
+     LoginFrame()
+    {
+        setSize(400,400);
 
         JLabel userLabel = new JLabel("User ID: ");
         JTextField userField = new JTextField(20);
         JLabel passLabel = new JLabel("Password: ");
         JPasswordField passField = new JPasswordField(20);
-        JButton resetBtn = new JButton("Reset TextFields");
+        JButton resetBtn = new JButton("Reset");
         JButton loginBtn = new JButton("Login");
+        JLabel welcomeLabel = new JLabel("Welcome to our Financial Portfolio Manager!");
+        JLabel helpLabel = new JLabel("Please login using your credentials.");
+        JLabel errorLabel = new JLabel();
         resetBtn.setSize(110, 70);
         loginBtn.setSize(110,70);
 
-        frame.add(userLabel);
-        frame.add(userField);
-        frame.add(passLabel);
-        frame.add(passField);
-        frame.add(resetBtn);
-        frame.add(loginBtn);
-        frame.setLayout(new FlowLayout());
-        frame.setTitle("Login");
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        JPanel panelOne = new JPanel();
+        JPanel panelTwo = new JPanel();
+        JPanel panelThree = new JPanel();
+        JPanel panelFour = new JPanel();
+        JPanel panelFive = new JPanel();
+        JPanel panelSix = new JPanel();
 
-        frame.setSize(400,400);
+        panelOne.add(userLabel);
+        panelOne.add(userField);
+        panelTwo.add(passLabel);
+        panelTwo.add(passField);
+        panelThree.add(resetBtn);
+        panelThree.add(loginBtn);
+        panelFour.add(errorLabel);
+        panelFive.add(welcomeLabel);
+        panelSix.add(helpLabel);
 
+
+        add(panelOne);
+        add(panelTwo);
+        add(panelThree);
+        add(panelFour);
+        add(panelFive);
+        add(panelSix);
+        setLayout(new FlowLayout());
+        setTitle("Financial Portfolio Manager Login");
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        resetBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                userField.setText("");
+                passField.setText("");
+            }
+        });
     }
 }
-public static void main( String args[] ){
-    LoginFrame frame = new LoginFrame();
-    frame.setVisible(true);
-    }
-}
-
-
